129cca06d3c6e8177509f2938676f0cd5bb96fdf
yefu <243249439@qq.com>
2025-06-05 00:32:32 +0800

    feat: 提交缺少的文件

---
+++ app/modules/Tool/DamageCalculator/black-list.ts
@@ -0,0 +1,31 @@
+export const DamageCalculatorBlackList: {
+  operator: {
+    [name: string]: {
+      skill: string[];
+    };
+  };
+} = {
+  operator: {
+    新约能天使: {
+      skill: ["skchr_angel2_1"],
+    },
+    Mon3tr: {
+      skill: ["skchr_monstr_1", "skchr_monstr_2"],
+    },
+    维娜·维多利亚: {
+      skill: ["skchr_siege2_1", "skchr_siege2_2"],
+    },
+    维什戴尔: {
+      skill: ["skchr_wisdel_1", "skchr_wisdel_2", "skchr_wisdel_3"],
+    },
+    玛恩纳: {
+      skill: ["skchr_mlynar_1", "skchr_mlynar_2"],
+    },
+    安洁莉娜: {
+      skill: ["skchr_aglina_1", "skchr_aglina_2", "skchr_aglina_3"],
+    },
+    逻各斯: {
+      skill: ["skchr_logos_1", "skchr_logos_2"],
+    },
+  },
+};
---
+++ app/modules/Tool/DamageCalculator/calculator/expression-util.ts
@@ -0,0 +1,59 @@
+import { type CalculatorInput } from "~/types/gameData";
+import { BuffContext } from "./buff-context";
+import { ExpressionGroupNode, NumericLiteralNode } from "./ast";
+
+/**
+ * 公式工具
+ */
+export class ExpressionUtil {
+  constructor(
+    public input: CalculatorInput,
+    public context: BuffContext,
+  ) {}
+
+  /** 干员局内攻击力 */
+  operator_in_game_atk() {
+    // 获取精英化等级属性
+    const { context, input } = this;
+    const attribute = input.charInput.phase?.attributesKeyFrames[input.charInput.level].data; // TODO 去掉?
+    const baseAtk = attribute?.atk ?? 0;
+
+    const outAtkExpression = new ExpressionGroupNode("*", "局外攻击力")
+      .addChild(
+        new ExpressionGroupNode("+", "局外加成")
+          .addChild(new NumericLiteralNode(baseAtk, "基础攻击力"))
+          .addChild(...context.relic_rune_add.atk_source.children),
+      )
+      .addChild(context.relic_rune_mul.atk_source);
+
+    const atk = new ExpressionGroupNode("*", "直接乘算")
+      .addChild(
+        new ExpressionGroupNode("+", "直接加算")
+          .addChild(outAtkExpression)
+          .addChild(...context.in_game_buff_add.atk_source.children),
+      )
+      .addChild(context.in_game_buff_mul.atk_source);
+
+    return new ExpressionGroupNode("*", "最终乘算")
+      .addChild(
+        new ExpressionGroupNode("+", "最终加算")
+          .addChild(atk)
+          .addChild(...context.in_game_buff_final_add.atk_source.children),
+      )
+      .addChild(...context.in_game_buff_final_mul.atk_source.children);
+  }
+
+  /** 敌人局内防御力 */
+  enemy_in_game_def() {
+    return new ExpressionGroupNode("*", "最终乘算")
+      .addChild(new NumericLiteralNode(this.input.enemyInput.attributes.def, "局外防御力"))
+      .addChild(...this.context.in_game_buff_final_mul.enemy_def_down_source.children);
+  }
+
+  /** 敌人法术抗性 */
+  // enemy_in_game_magic_resistance() {
+  //   return new ExpressionGroupNode("*", "最终乘算")
+  //     .addChild(new NumericLiteralNode(this.input.enemyInput.attributes.def, "局外防御力"))
+  //     .addChild(...this.context.in_game_buff_final_mul.enemy_def_down_source.children);
+  // }
+}

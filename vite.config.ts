a01408e5b06ad65eeca4c777ade167ada43581f5
yefu <243249439@qq.com>
2026-02-07 17:59:55 +0800

    feat: 依赖升级(tailwindcss4, heroui, vite) 保留部分待迁移文件

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { reactRouter } from "@react-router/dev/vite";
-import autoprefixer from "autoprefixer";
-import tailwindcss from "tailwindcss";
+// import autoprefixer from "autoprefixer";
+import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
@@ -21,15 +21,15 @@ export default defineConfig({
     },
   },
   // publicDir: false,
-  css: {
-    postcss: {
-      plugins: [tailwindcss, autoprefixer],
-    },
-    devSourcemap: true,
-  },
+  // css: {
+  //   postcss: {
+  //     plugins: [tailwindcss, autoprefixer],
+  //   },
+  //   devSourcemap: true,
+  // },
   build: {
     sourcemap: "hidden",
   },
-  plugins: [reactRouter(), tsconfigPaths()],
+  plugins: [tailwindcss(), reactRouter(),  tsconfigPaths()],
 });
 

2e4eff921ba6f5784ebaa58a3fe9ef577f8791c0
teresaliu1212 <teresaliu1212@gmail.com>
2026-08-04 01:02:32 -0700

    fix(map)：第n+6次修改地图数据呜呜

--- app/modules/Tool/BlackFlowMap/mapData.tsx
+++ app/modules/Tool/BlackFlowMap/mapData.tsx
@@ -58,7 +58,7 @@ export const nodeOptions: nodeOption[] = [
     name: "“居民”据点",
     steps: [
       { zone: "zone_2", min: 4, max: 7, maxAllowed: 1 },
-      { zone: "zone_4", min: 4, max: 13, maxAllowed: 1 },
+      { zone: "zone_4", min: 4, max: 14, maxAllowed: 1 },
       { zone: "zone_5", min: 4, max: 11, maxAllowed: 1 },
     ],
     type: "battle",
@@ -92,7 +92,7 @@ export const nodeOptions: nodeOption[] = [
     name: "应急助力",
     steps: [
       { zone: "zone_2", min: 2, max: 5, maxAllowed: 1 },
-      { zone: "zone_3", min: 4, max: 11, maxAllowed: 1 },
+      { zone: "zone_3", min: 4, max: 12, maxAllowed: 1 },
       { zone: "zone_4", min: 2, max: 3, maxAllowed: 1 },
       { zone: "zone_5", min: 3, max: 5, maxAllowed: 1 },
     ],
@@ -105,7 +105,7 @@ export const nodeOptions: nodeOption[] = [
       { zone: "zone_1", min: 2, max: 5, maxAllowed: 2 },
       { zone: "zone_2", min: 2, max: 7, maxAllowed: 2 },
       { zone: "zone_3", min: 1, max: 12, maxAllowed: 3 },
-      { zone: "zone_4", min: 1, max: 10, maxAllowed: 3 },
+      { zone: "zone_4", min: 1, max: 11, maxAllowed: 3 },
       { zone: "zone_5", min: 1, max: 10, maxAllowed: 3 },
     ],
     type: "other",
